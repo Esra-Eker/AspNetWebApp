0964335d9ce877c0b4eb9db0d5a7fbd0fceea5b2
Esra <elifesraeker@gmail.com>
2023-03-10 16:50:46 +0300

    UnitOfWork eklendi

---
+++ NLayer.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayer.Core.UnitOfWorks;
+
+namespace NLayer.Repository.UnitOfWorks
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CommitAync()
+        {
+            _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
