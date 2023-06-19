7dc6342c77273e2560bbf49d084c2a7d32b21fa2
Esra <elifesraeker@gmail.com>
2023-06-19 10:56:39 +0300

    ProductsController Save

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -1,5 +1,9 @@
 ﻿
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
 using NLayer.Core.Services;
 
 namespace NLayer.Web.Controllers
@@ -7,6 +11,8 @@ namespace NLayer.Web.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _services;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
         public ProductsController(IProductService services)
         {
@@ -17,6 +23,35 @@ namespace NLayer.Web.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _services.GetProductsWithCategory());
+        }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = _categoryService.GetAllAsync();
+            
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");   
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto products)
+        {
+            if (ModelState.IsValid)
+            {
+                await _services.AddAsync(_mapper.Map<Product>(products));
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = _categoryService.GetAllAsync();
+            
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+
+            return View();
         }
     }
 }
