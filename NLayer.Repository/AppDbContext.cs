901d7afe0d69dd2ca8bad5bf9ff45c6e6792ac8a
Esra <elifesraeker@gmail.com>
2023-09-04 16:51:38 +0300

    CreatedDate-UpdateDate

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 using Microsoft.EntityFrameworkCore;
 using NLayer.Core.Models;
 
@@ -14,6 +16,56 @@ namespace NLayer.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                        {
+                            entityReference.CreatedDate=DateTime.UtcNow;
+                            break;
+                        }
+                        case EntityState.Modified:
+                        {
+                            Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                            entityReference.UpdatedDate=DateTime.UtcNow;
+                            break;
+                        }
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                        {
+                            entityReference.CreatedDate=DateTime.UtcNow;
+                            break;
+                        }
+                        case EntityState.Modified:
+                        {
+                            Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                            entityReference.UpdatedDate=DateTime.UtcNow;
+                            break;
+                        }
+                    }
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
